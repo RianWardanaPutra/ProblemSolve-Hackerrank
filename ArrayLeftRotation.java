b931ba342f0120e2cecd98760bbcebb8e82f1a22
alienatedDaddy <rian.w@mail.ugm.ac.id>
2019-07-16 13:18:23 +0700

    added arrayleftrotation

---
+++ ArrayLeftRotation.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] nd = sc.nextLine().split(" ");
+        int n = Integer.parseInt(nd[0]);
+        int d = Integer.parseInt(nd[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = sc.nextLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(arrItems[i]);
+        }
+
+        int start = n % d;
+
+        int[] newArr = new int[n];
+
+        int count = 0;
+
+        for(int i = d; i < n; i++) {
+            newArr[count] = arr[i];
+            count++;
+        }
+
+        for (int i = 0; i < n - (n - d); i++) {
+            newArr[count] = arr[i];
+            count++;
+        }
+
+        for (int a : newArr) {
+            System.out.print(a + " ");
+        }
+
+        sc.close();
+    }
+}
